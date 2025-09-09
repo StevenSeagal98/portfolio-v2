88dfa222f50055299a71d3df94f9e354a1fa48c6
nateorona98 <nathaniel.orona98@gmail.com>
2025-09-09 01:10:49 -0500

    main

--- src/components/home/HomeMain.tsx
+++ src/components/home/HomeMain.tsx
@@ -1,6 +1,5 @@
 import HomeHero from '@/components/home/HomeHero';
 import HomeProjects from '@/components/home/HomeProjects';
-import HomeBg from '@/components/home/HomeBg';
 import HomeAbout from '@/components/home/HomeAbout';
 import HomeSkills from '@/components/home/HomeSkills';
 import HomeContact from '@/components/home/HomeContact';
@@ -9,7 +8,6 @@ import HomeExperience from '@/components/home/HomeExperience';
 const HomeMain = () => {
     return (
         <main className="container mx-auto px-4">
-            {/* <HomeBg /> */}
             <HomeHero />
             <HomeAbout />
             <HomeSkills />
